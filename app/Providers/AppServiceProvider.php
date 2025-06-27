c7325a3bc8e5f9033dcfe9c41c0afd7d750cfab8
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-06-27 21:06:32 +0700

    feat: add Blade directive for role-based access control

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::if('roles', function (array $roles) {
+            return auth()->check() && in_array(auth()->user()->role, $roles, true);
+        });
+
+        Blade::if('isguest', function () {
+            return !auth()->check();
+        });
+
     }
 }
