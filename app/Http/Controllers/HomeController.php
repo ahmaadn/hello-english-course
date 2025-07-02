15f78e961101855c3f7c10caa2cd2b78d214fdc3
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-07-03 00:32:53 +0700

    feat: update about page to display team members with images and details

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,39 @@ class HomeController extends Controller
 
     public function about()
     {
-        return view("pages.about");
+        $team = [
+            [
+                'nama' => 'Agisni Maulani Hafitri',
+                'nim' => '2206149',
+                'img' => 'img/people/1.png',
+            ],
+            [
+                'nama' => 'Muhamad Chikal Ubaidillah Nurhasan',
+                'nim' => '2306158',
+                'img' => 'img/people/2.jpg',
+            ],
+            [
+                'nama' => 'Muhammad Saiful Rizal',
+                'nim' => '2206164',
+                'img' => 'img/people/3.jpeg',
+            ],
+            [
+                'nama' => 'Asep Abdul Rohman',
+                'nim' => '2206161',
+                'img' => 'img/people/4.jpeg',
+            ],
+            [
+                'nama' => 'Vito Gunawan',
+                'nim' => '2306149',
+                'img' => 'img/people/5.jpeg',
+            ],
+            [
+                'nama' => 'Bubu Bukhori Muslim',
+                'nim' => '2306156',
+                'img' => 'img/people/6.jpeg',
+            ],
+        ];
+        return view("pages.about", compact('team'));
     }
 
     public function modules()
