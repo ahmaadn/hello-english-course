c066eb3b513895531a390589877f8e577cb737f2
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-07-03 00:00:38 +0700

    Add Vercel configuration and initial project setup (#1)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Blade::if('roles', function (array $roles) {
             return auth()->check() && in_array(auth()->user()->role, $roles, true);
         });
